48e53296c5216edebe134423f4bd6f55423f2819
zahramajd <zahra.majdabadi95@gmail.com>
2019-06-16 01:19:30 +0430

    load data

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
  # headline generation
+
+
+
+
+# load data
+with open("HEADLINE_Generation/sample.headline.data", "r") as fp:
+    headlines = fp.readlines()
+    fp.close()
+
+with open("HEADLINE_Generation/sample.content.data", "r") as fp:
+    contents = fp.readlines()
+    fp.close()
+
+
